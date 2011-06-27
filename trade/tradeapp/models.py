3db59d6567f151ad5e847ca582d8c2864ba62a49
selNew <ksn900@gmail.com>
2011-06-27 11:44:22 +0000

    Added admin function to model

--- trade/tradeapp/models.py
+++ trade/tradeapp/models.py
@@ -8,20 +8,30 @@ class Advert(models.Model):
 	price=models.FloatField()
 	phone=models.CharField()
 	email=models.EmailField()
-	Created=models.DateTimeField(auto_now_add=True)
-	Updated=models.DateTimeField(auto_now=True)
+	created=models.DateTimeField(auto_now_add=True)
+	updated=models.DateTimeField(auto_now=True)
 	def __unicode__(self):
 	    return self.prodName+ " , "+self.price+" ," +str(self.Updated)
 
 class Request(models.Model):
 	product=models.CharField(max_length=100)
-	Created=models.DateTimeField(auto_now_add=True)
-	Updated=models.DateTimeField(auto_now=True)
+	created=models.DateTimeField(auto_now_add=True)
+	updated=models.DateTimeField(auto_now=True)
 	phone=models.CharField()
 	email=models.EmailField()
 	def __unicode__(self):
 	    return self.product+" , "+str(self.Updated)
 
+class AdvertAdmin(admin.ModelAdmin):
+       list_display=('prodName','name','price','phone','email','created')
+       list_filter=('prodName','created','name')
+       search_fields=('name','prodName')
+       ordering=('prodName','created')
 
+class RequestAdmin(admin.modelAdmin):
+      list_display=('product','created','phone','email')
+      ordering=('product','-created')
 
+admin.site.register(Advert,AdvertAdmin)
+admin.site.register(Request, RequestAdmin)
 	
