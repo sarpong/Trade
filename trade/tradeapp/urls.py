47d235cfe8388841aa312901eabf41f7107fa2d5
Fleischer <Spefben89@yahoo.com>
2011-06-27 15:49:15 +0000

    corrected urls.py

--- trade/tradeapp/urls.py
+++ trade/tradeapp/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('',
 	url(r'^$','tradeapp.views.home')
 	url(r'^adlist/$', 'tradeapp.views.advert_list'),
 	url(r'^reqlist/$', 'tradeapp.views.request_list'),
-	url(r'^search/(.*)$','tradeapp.views.search')
+	url(r'^search/(.*)$','tradeapp.views.search'),
 #	url(r'^addetail/(?P<id>\d+)/((?P<showAdvert>.*)/)?$','tradeapp.views.advert_detail')
 #	url(r'^reqdetail/(?P<id>\d+)/((?P<showRequest>.*)/)?$','tradeapp.views.request_detail')
 #	url(r'^logout/$','tradeapp.views.logoutView')
