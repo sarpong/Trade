0d2e499ffe3498019782396378e85c25e162a193
sarpong <kaysarP@yahoo.com>
2011-06-27 15:26:00 +0400

    changed item to product

--- trade/tradeapp/models.py
+++ trade/tradeapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib import admin
 
 class Advert(models.Model):
-	Item=models.CharField(max_length=100)
+	product=models.CharField(max_length=100)
 	name=models.CharField(max_length=60)
 	price=models.FloatField()
 	phone=models.CharField(max_length=100)
@@ -10,26 +10,26 @@ class Advert(models.Model):
 	created=models.DateTimeField(auto_now_add=True)
 	updated=models.DateTimeField(auto_now=True)
 	def __unicode__(self):
-	    return self.Item+ " , $"+str(self.price)+" ," +str(self.updated)
+	    return self.product+ " , $"+str(self.price)+" ," +str(self.updated)
 
 class Request(models.Model):
-	Item=models.CharField(max_length=100)
+	product=models.CharField(max_length=100)
 	created=models.DateTimeField(auto_now_add=True)
 	updated=models.DateTimeField(auto_now=True)
 	phone=models.CharField(max_length=100)
 	email=models.EmailField()
 	def __unicode__(self):
-	    return self.Item+" , "+str(self.updated)
+	    return self.product+" , "+str(self.updated)
 
 class AdvertAdmin(admin.ModelAdmin):
-       list_display=('Item','name','price','phone','email','created')
-       list_filter=('Item','created','name')
-       search_fields=('name','Item')
-       ordering=('Item','created')
+       list_display=('product','name','price','phone','email','created')
+       list_filter=('product','created','name')
+       search_fields=('name','product')
+       ordering=('product','created')
 
 class RequestAdmin(admin.ModelAdmin):
-      list_display=('Item','created','phone','email')
-      ordering=('Item','-created')
+      list_display=('product','created','phone','email')
+      ordering=('product','-created')
 
 admin.site.register(Advert,AdvertAdmin)
 admin.site.register(Request,RequestAdmin)
