24937dbb54804ca7d460309c9f01c48553eb80e3
Fleischer <Spefben89@yahoo.com>
2011-06-27 12:51:47 +0000

    changed ignore and settings

--- trade/settings.py
+++ trade/settings.py
@@ -1,3 +1,7 @@
+import os
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+ 
+
 # Django settings for trade project.
 
 DEBUG = True
@@ -103,11 +107,8 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'trade.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates')
 )
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
