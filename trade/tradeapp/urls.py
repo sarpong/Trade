2a0d0c747d586a0dd164b47b84e877d1fce8d8fa
Fleischer <Spefben89@yahoo.com>
2011-06-27 15:51:48 +0000

    corrected urls.py

--- trade/tradeapp/urls.py
+++ trade/tradeapp/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-	url(r'^$','tradeapp.views.home')
+	url(r'^$','tradeapp.views.home'),
 	url(r'^adlist/$', 'tradeapp.views.advert_list'),
 	url(r'^reqlist/$', 'tradeapp.views.request_list'),
 	url(r'^search/(.*)$','tradeapp.views.search'),
-#	url(r'^addetail/(?P<id>\d+)/((?P<showAdvert>.*)/)?$','tradeapp.views.advert_detail')
-#	url(r'^reqdetail/(?P<id>\d+)/((?P<showRequest>.*)/)?$','tradeapp.views.request_detail')
-#	url(r'^logout/$','tradeapp.views.logoutView')
-#	url(r'^login$','tradeapp.views.loginView')
-#	url(r'^editreq$','tradeapp.views.editreq')
-#	url(r'^editadvert$','tradeapp.views.editad')
+#	url(r'^addetail/(?P<id>\d+)/((?P<showAdvert>.*)/)?$','tradeapp.views.advert_detail'),
+#	url(r'^reqdetail/(?P<id>\d+)/((?P<showRequest>.*)/)?$','tradeapp.views.request_detail'),
+#	url(r'^logout/$','tradeapp.views.logoutView'),
+#	url(r'^login$','tradeapp.views.loginView'),
+#	url(r'^editreq$','tradeapp.views.editreq'),
+#	url(r'^editadvert$','tradeapp.views.editad'),
 )
 
